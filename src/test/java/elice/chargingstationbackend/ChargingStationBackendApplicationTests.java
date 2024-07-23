e1f1deb39a9f8bd8899eb04b9d55831fe969057f
zkan98 <anzk9898@gmail.com>
2024-07-23 15:52:16 +0900

    Initial commit

---
+++ src/test/java/elice/chargingstationbackend/ChargingStationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package elice.chargingstationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargingStationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
