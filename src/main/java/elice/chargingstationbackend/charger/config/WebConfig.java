7ee2296af7b5aad293026ba96ef605ca7ded7a8f
Doohyun Kim <engus7303>
2024-08-08 22:16:41 +0900

    fix:주변 충전소 조회

--- src/main/java/elice/chargingstationbackend/charger/config/WebConfig.java
+++ src/main/java/elice/chargingstationbackend/charger/config/WebConfig.java
@@ -1,16 +1,16 @@
-package elice.chargingstationbackend.charger.config;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // React 개발 서버의 주소를 지정합니다.
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
-    }
-}
+//package elice.chargingstationbackend.charger.config;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:5173") // React 개발 서버의 주소를 지정합니다.
+//                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                .allowedHeaders("*");
+//    }
+//}
