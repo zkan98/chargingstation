363e4a8f1673ab8ab5e7ec1c79b1eb15996f9534
zkan98 <anzk9898@gmail.com>
2024-08-14 20:45:04 +0900

    delete env

--- src/main/java/elice/chargingstationbackend/ChargingStationBackendApplication.java
+++ src/main/java/elice/chargingstationbackend/ChargingStationBackendApplication.java
@@ -1,6 +1,5 @@
 package elice.chargingstationbackend;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChargingStationBackendApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().load();
-
         SpringApplication.run(ChargingStationBackendApplication.class, args);
     }
 
